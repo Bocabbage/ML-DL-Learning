20fdb7ada0057105df6d8fd0ee66028f428b4e66
Bocabbage <378066505@qq.com>
2019-03-14 00:04:09 +0800

    Update 'PCA.py',adding the 'svd' method.

--- Python/PCA.py
+++ Python/PCA.py
@@ -1,6 +1,7 @@
 # 利用numpy实现PCA
 # 更新时间：2019/3/12
 #          2019/3/13(Debug,处理中心化)
+#          2019/3/14(SVD方案，未处理完全)
 import numpy as np
 from mlxtend.data import loadlocal_mnist
 import matplotlib.pyplot as plt
@@ -8,27 +9,37 @@ from sklearn.decomposition import PCA
 
 class PCA_2d:
     """Take the data forms as X(m*n) to do PCA"""
-    def __init__(self,n_components):
+    def __init__(self,n_components,use_svd='eig'):
         """Let the n-dim data to be n_components-dim"""
         self.n_components = n_components
+        self.use_svd = use_svd
     def fit(self,X):
         """X is a 2-d numpy array"""
         mean_X = np.mean(X,axis = 0) 
         X_n = X - mean_X
-        sigma = np.dot(X_n.T,X_n)
-        #sigma = np.cov(X.T)
-        eigvalue,eigvector = np.linalg.eig(sigma)
-        eigvalue = np.real(eigvalue)
-        eigvector = np.real(eigvector)
-        self.w = eigvector[np.argsort(-eigvalue)[:self.n_components],:]
+        if self.use_svd == 'eig':
+            sigma = np.dot(X_n.T,X_n)
+            #sigma = np.cov(X.T)
+            eigvalue,eigvector = np.linalg.eig(sigma)
+            eigvalue = np.real(eigvalue)
+            eigvector = np.real(eigvector)
+            self.w = eigvector[np.argsort(-eigvalue)[:self.n_components],:]
+        elif self.use_svd == 'svd':
+            self.U,self.S,V = np.linalg.svd(X_n)
+            self.w = self.U[:,0:self.n_components]
+
+
     def transform(self,Y):
-        return np.dot(Y,self.w.T)
+        if self.use_svd == 'eig':
+            return np.dot(Y,self.w.T)
+        elif self.use_svd == 'svd':
+            return self.w * self.S[:self.n_components]
 
 # Test
 tr_data,tr_labels = loadlocal_mnist(images_path='E:/MNIST/train-images.idx3-ubyte',
                                     labels_path='E:/MNIST/train-labels.idx1-ubyte') 
 nc = 15
-pca = PCA_2d(nc*nc)
+pca = PCA_2d(nc*nc,"svd")
 pca.fit(tr_data[0:1000,:])
 
 pca_sk = PCA(n_components=nc*nc,svd_solver="full")
