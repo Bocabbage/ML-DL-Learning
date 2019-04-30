f675f313f94403efd6a2b00bf4b70ca9b6afb86f
Bocabbage <378066505@qq.com>
2019-04-30 10:23:55 +0800

    Update AnimeFacesGen.

--- Python/pytorch/AnimeFacesGen/DCGAN.py
+++ Python/pytorch/AnimeFacesGen/DCGAN.py
@@ -11,13 +11,21 @@ import os
 
 #Hyper parameters
 IS_CUDA = True
-NGPU = 1
 DATASET_PATH = 'E:/Programming/Dataset/AnimeFaces/'
 IMAGE_SIZE = 64
 BATCH_SIZE = 50
-LR_G = 0.0001
-LR_D = 0.0001
+LR_G = 0.0002
+LR_D = 0.0002
+BETAS = (0.5,0.999)
 EPOCH = 1
+
+# GPU Setting
+NGPU = 0
+if torch.cuda.is_available():
+    NGPU = 1
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
 
 
 # Load Dataset
@@ -35,6 +43,7 @@ data_loader = torch.utils.data.DataLoader(dataset=dataset,
 
 # data check
 def imshow(img):
+    img = img/2 + 0.5
     npimg = img.numpy()
     print(npimg.shape)
     plt.imshow(np.transpose(npimg,(1,2,0)))
@@ -45,8 +54,8 @@ images, _ = data_iter.next()
 print(images.numpy()[0].shape)
 imshow(torchvision.utils.make_grid(images))
 
-# Module Define
-class Generator(nn.module):
+# Module Define (DCGAN)
+class Generator(nn.Module):
     def __init__(self,ngpu):
         super(Generator,self).__init__()
         self.ngpu = ngpu
@@ -160,4 +169,63 @@ class Discriminator(nn.Module):
             output = self._main(input)
 
         return ouput.view(-1,1).squeeze(1)
+
+print(device)
+
+netG = Generator(NGPU).to(device)
+print(netG)
+
+netD = Discriminator(NGPU).to(device)
+print(netD)
+
+# Criterion and Optimizer options
+criterion = nn.BCELoss()
+real_label = 1
+fake_label = 0
+
+optimizerD = optim.Adam(netD.parameters(),lr=LR_D,betas=BETAS)
+optimizerG = optim.Adam(netG.parameters(),lr=LR_G,betas=BETAS)
+
+# for epoch in range(EPOCH):
+#     for batch_idx,data in enumerate(data_loader):
+#         ###### Step1: Update Discriminator ######
+#         ### Maximize log(D(x))+log(1-D(G(z))) ###
+#         #########################################
+#         # train with real #
+#         netD.zero_grad()
+#         real_data = data[0].to(device)
+#         batch_size = real_data.size(0)
+#         labels = torch.full((batch_size,),real_label,device=device)
+
+#         output = netD(real_data)
+#         errD_real = criterion(output,labels)
+#         errD_real.backward()
+
+#         # train with fake #
+#         noise = torch.randn(batch_size,NZ,1,1,device=device)
+#         fake = netG(noise)
+#         # replace the 'labels' with fake_label in-place
+#         labels.fill_(fake_label)
+#         # detach it so it won't influence the parameters in G
+#         output = netD(fake.detach())
+#         errD_fake = criterion(output,labels)
+#         errD_fake.backward()
+
+#         errD = errD_real + errD_fake
+#         optimizerD.step()
+
+#         #########################################
+
+#         ###### Step2: Update Generator ######
+#         ###### Maximize log(D(G(z)))   ######
+#         #####################################
+#         netG.zero_grad()
+#         labels.fill_(real_label)
+#         output = netD(fake)
+#         errG = criterion(output,labels)
+#         errG.backward()
+
+#         optimizerG.step()
+
+#         #####################################
 
---
+++ Python/pytorch/AnimeFacesGen/README.md
@@ -0,0 +1,10 @@
+# AnimeFacesGenerate
+
+## Target
+Use DCGAN for Anime-faces Generation.
+
+## Dataset
+![image](imgs/DatasetSamples.png)
+
+## Instruments
+* pytorch
